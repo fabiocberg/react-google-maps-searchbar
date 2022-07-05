c5169d7cedca59d599364f5118b68db73e1f2c1e
Fabio Bergmann <fabiocberg@gmail.com>
2022-07-05 04:15:43 -0300

    google maps with searchbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import {
+    GoogleMap,
+    LoadScript,
+    Marker,
+    StandaloneSearchBox,
+} from "@react-google-maps/api";
+import { useState } from "react";
+
+const libraries = ["places"];
+
+const position = {
+    lat: -27.590824,
+    lng: -48.551262,
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [searchBox, setSearchBox] = useState();
+    const [map, setMap] = useState();
+    const [point, setPoint] = useState();
+
+    const onPlacesChanged = () => {
+        const places = searchBox.getPlaces();
+        const place = places[0];
+        const location = {
+            lat: place.geometry.location.lat(),
+            lng: place.geometry.location.lng(),
+        };
+        setPoint(location);
+        console.log(place);
+        console.log(location);
+        map.panTo(location);
+    };
+
+    return (
+        <div className="map">
+            <LoadScript
+                googleMapsApiKey="AIzaSyA-rzIIZK2SjyZqBTDaeO9Q16EGfceGnBw"
+                libraries={libraries}
+            >
+                <GoogleMap
+                    onLoad={(ref) => setMap(ref)}
+                    mapContainerStyle={{ width: "100%", height: "100%" }}
+                    center={position}
+                    zoom={15}
+                >
+                    <div className="address">
+                        <StandaloneSearchBox
+                            onLoad={(ref) => setSearchBox(ref)}
+                            onPlacesChanged={onPlacesChanged}
+                        >
+                            <input
+                                className="addressField"
+                                placeholder="Type the address here."
+                            />
+                        </StandaloneSearchBox>
+                        {point && <Marker position={point} />}
+                    </div>
+                </GoogleMap>
+            </LoadScript>
+        </div>
+    );
 }
 
 export default App;
