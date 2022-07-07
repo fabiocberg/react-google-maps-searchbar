5fa000434ac897561294462105185bc2ad85acb2
Fabio Bergmann <fabiocberg@gmail.com>
2022-07-07 20:05:41 -0300

    feat: firestore, react-datepicker

--- src/App.js
+++ src/App.js
@@ -5,7 +5,23 @@ import {
     Marker,
     StandaloneSearchBox,
 } from "@react-google-maps/api";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+
+import {
+    addDoc,
+    collection,
+    orderBy,
+    getDocs,
+    query,
+    serverTimestamp,
+    where,
+} from "firebase/firestore";
+
+import { firestore } from "./index";
+
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
 
 const libraries = ["places"];
 
@@ -18,6 +34,50 @@ function App() {
     const [searchBox, setSearchBox] = useState();
     const [map, setMap] = useState();
     const [point, setPoint] = useState();
+
+    const [name, setName] = useState("");
+    const [date, setDate] = useState(new Date());
+
+    const [initialLoading, setInitialLoading] = useState(true);
+    const [updateEvents, setUpdateEvents] = useState(true);
+    const [events, setEvents] = useState();
+
+    const eventsRef = collection(firestore, "events");
+
+    const getEvents = useCallback(() => {
+        console.log("getEvents");
+        if (updateEvents) {
+            console.log("getEvents2");
+            setUpdateEvents(false);
+            const dt = new Date();
+            dt.setDate(dt.getDate() - 1);
+            console.log(dt.toISOString());
+            const q = query(
+                eventsRef,
+                where("date", ">", dt),
+                orderBy("date", "asc")
+            );
+            getDocs(q)
+                .then((docs) => {
+                    console.log(docs);
+                    setEvents(docs);
+                })
+                .catch((err) => console.log(err))
+                .finally(() => console.log("get docs done."));
+        }
+    }, [eventsRef, updateEvents]);
+
+    const doGetEvents = useCallback(() => {
+        setUpdateEvents(true);
+        getEvents();
+    }, [getEvents]);
+
+    useEffect(() => {
+        if (initialLoading) {
+            setInitialLoading(false);
+            doGetEvents();
+        }
+    }, [doGetEvents, initialLoading]);
 
     const onPlacesChanged = () => {
         const places = searchBox.getPlaces();
@@ -32,10 +92,28 @@ function App() {
         map.panTo(location);
     };
 
+    const saveEvent = (e) => {
+        e.preventDefault();
+        const newEvent = {
+            name,
+            date,
+            location: point,
+            createdAt: serverTimestamp(),
+            updatedAt: serverTimestamp(),
+        };
+        setName("");
+        setDate(new Date());
+        addDoc(eventsRef, newEvent)
+            .then(() => {
+                console.log("document created");
+            })
+            .catch((err) => console.log(err));
+    };
+
     return (
         <div className="map">
             <LoadScript
-                googleMapsApiKey="AIzaSyA-rzIIZK2SjyZqBTDaeO9Q16EGfceGnBw"
+                googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
                 libraries={libraries}
             >
                 <GoogleMap
@@ -45,17 +123,54 @@ function App() {
                     zoom={15}
                 >
                     <div className="address">
-                        <StandaloneSearchBox
-                            onLoad={(ref) => setSearchBox(ref)}
-                            onPlacesChanged={onPlacesChanged}
-                        >
+                        <form onSubmit={saveEvent}>
+                            <StandaloneSearchBox
+                                onLoad={(ref) => setSearchBox(ref)}
+                                onPlacesChanged={onPlacesChanged}
+                            >
+                                <input
+                                    className="addressField"
+                                    placeholder="Type the address here."
+                                />
+                            </StandaloneSearchBox>
                             <input
-                                className="addressField"
-                                placeholder="Type the address here."
+                                type="text"
+                                placeholder="Name"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
                             />
-                        </StandaloneSearchBox>
-                        {point && <Marker position={point} />}
+                            <DatePicker
+                                selected={date}
+                                onChange={(date) => setDate(date)}
+                            />
+                            <button type="submit">Save</button>
+                        </form>
+                        <div style={{ fontSize: 10, marginTop: 16 }}>
+                            {events?.docs?.map((doc) => {
+                                const event = doc.data();
+                                return (
+                                    <div
+                                        key={doc.id}
+                                        style={{ marginBottom: 10 }}
+                                    >
+                                        <div>
+                                            <strong>Name:</strong> {event.name}
+                                        </div>
+                                        <div>
+                                            <strong>Date:</strong>{" "}
+                                            {event.date.toDate().toISOString()}
+                                        </div>
+                                        <div>
+                                            <strong>Location:</strong> lat(
+                                            {event.location.lat}), lng(
+                                            {event.location.lng})
+                                        </div>
+                                    </div>
+                                );
+                            })}
+                        </div>
                     </div>
+                    {point && <Marker position={point} />}
                 </GoogleMap>
             </LoadScript>
         </div>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// export const analytics = getAnalytics(app);
+export const firestore = getFirestore(app);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
